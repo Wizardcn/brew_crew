bba7a0cc3c135760844a9fce24b3afc480e1164f
TONY <supawat125947@gmail.com>
2022-06-30 10:31:53 +0700

    comment out firestore service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -6,19 +6,19 @@ class DatabaseService {
 
   // collection reference: we didn't create brews collection, so when the time this code runs that collection doesn't exist
   // firestore will go ahead and create anyway.
-  final CollectionReference brewCollection =
-      FirebaseFirestore.instance.collection('brews');
+  // final CollectionReference brewCollection =
+  //     FirebaseFirestore.instance.collection('brews');
 
-  Future updateUserData(String sugars, String name, int strength) {
-    return brewCollection.doc(uid).set({
-      'sugars': sugars,
-      'name': name,
-      'strength': strength,
-    });
-  }
+  // Future updateUserData(String sugars, String name, int strength) {
+  //   return brewCollection.doc(uid).set({
+  //     'sugars': sugars,
+  //     'name': name,
+  //     'strength': strength,
+  //   });
+  // }
 
-  //get brews stream
-  Stream<QuerySnapshot> get brews {
-    return brewCollection.snapshots();
-  }
+  // just get brews stream
+  // Stream<QuerySnapshot> get brews {
+  //   return brewCollection.snapshots();
+  // }
 }
