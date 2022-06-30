44410ded334655517213b67040e64f519395824e
TONY <supawat125947@gmail.com>
2022-06-30 10:32:51 +0700

    add audio player page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/screens/home/audio_player.dart';
 import 'package:brew_crew/screens/home/brew_list.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -36,7 +37,7 @@ class Home extends StatelessWidget {
             )
           ],
         ),
-        body: BrewList(),
+        body: const audioScreen(),
       ),
     );
   }
